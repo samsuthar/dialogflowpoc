fc46828134ed828baff857b633ff01b4d785e6fd
satya <satya@DESKTOP-720DMFM>
2018-06-15 12:59:43 -0400

    Dialogflow Node js Client - ANthem POC

---
+++ config.js
@@ -0,0 +1,21 @@
+export default {
+    app: {
+        token: "cec95b7129224e1590817df5bdb9e699", // <- enter your token here
+        muted: false, // <- mute microphone by default
+        googleIt: true // <- ask users to google their request, in case of input.unknown action
+    },
+    locale: {
+        strings: {
+            welcomeTitle: "Welcome to Anthem!   My name is Dina.  How can i help you?",
+            welcomeDescription: `You can type "Hello" for example. Or just press on the microphone to talk`,
+            offlineTitle: "Oh, no!",
+            offlineDescription: "It looks like you are not connected to the internet, this webpage requires internet connection, to process your requests",
+            queryTitle: "Ask me anything you would like to know...",
+            voiceTitle: "Go ahead, im listening..."
+        },
+        settings: {
+            speechLang: "en-GB", // <- output language
+            recognitionLang: "en-US" // <- input(recognition) language
+        }
+    }
+}
